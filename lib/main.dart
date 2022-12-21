15cbd3f330c71c7d20a16cf38cb6333b9e97c47c
Ahot0110 <thoathoa0110@gmail.com>
2022-12-22 03:37:10 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:chat_app_flutter/constants/app_theme.dart';
+import 'package:chat_app_flutter/constants/strings.dart';
+import 'package:chat_app_flutter/pages/landing_page.dart';
+import 'package:chat_app_flutter/services/auth.dart';
+import 'package:chat_app_flutter/services/notification.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+RemoteMessage? initMsg;
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
+Future<void> main() async {
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    initMsg = await FirebaseMessaging.instance.getInitialMessage();
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    runApp(MyApp());
+  },
+      (error, stack) =>
+          FirebaseCrashlytics.instance.recordError(error, stack, fatal: true));
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  final NotificationBase notification = FirebaseNotification()..initialize();
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthBase>(create: (_) => Auth()),
+        Provider<NotificationBase>(create: (_) => notification),
+      ],
+      child: MaterialApp(
+        title: Strings.appName,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.system,
+        home: const LandingPage(),
+      ),
+    );
+  }
+}
---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,43 @@
+import 'package:chat_app_flutter/pages/home/home_page.dart';
+import 'package:chat_app_flutter/pages/login/login_page.dart';
+import 'package:chat_app_flutter/services/auth.dart';
+import 'package:chat_app_flutter/services/database.dart';
+import 'package:chat_app_flutter/services/notification.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
+
+    ScreenUtil.init(
+      context,
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      // orientation: Orientation.portrait,
+      
+    );
+    return StreamBuilder<User?>(
+      stream: auth.authStateChanges(),
+      builder: (context, snapshot) {
+        final User? user = snapshot.data;
+        if (user == null) {
+          return LoginPage(auth: auth);
+        } else {
+          return Provider<Database>(
+            /// provider database and save new msg token (device token) to storage
+            create: (_) => FireStoreDatabase(uid: user.uid)
+              ..addMsgToken(token: currentDeviceMsgToken),
+            child: HomePage(),
+          );
+        }
+      },
+    );
+  }
+}
